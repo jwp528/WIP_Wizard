f2a6aa630ed397a8e40e8c712479c00db9d21f77
Josh Parsons <jparsons@dragoncore.ca>
2023-06-17 15:49:52 -0300

    Added a basic shared library

---
+++ Shared/Models/BaseObject.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class BaseObject
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public string CreatedBy { get; set; }
+
+    public DateTime UpdatedOn { get; set; }
+
+    public string UpdatedBy { get; set; }
+
+    public DateTime? FlaggedOn { get; set; }
+
+    public FlagEnum? FlaggedAs { get; set; }
+}
